70476c71a7b92ca1c230cb1118addc31e435faf0
AshirArfan <u2022124@giki.edu.pk>
2024-08-28 11:19:55 +0500

    ip

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg');
 // Database connection
 const pool = new Pool({
   user: 'postgres',
-  host: '172.19.0.2',
+  host: '172.31.62.172',
   database: 'test',
   password: 'postgres',  // Replace with your PostgreSQL password
   port: 5432,
